b3040f25d915437ed85177416f6350ce8e11d264
KanzFaras <faraskanz11@gmail.com>
2024-08-06 11:48:06 +0700

    Initial Commit

---
+++ Basic_Knowledge/Basic_KnowledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_KnowledgeApp.swift
+//  Basic_Knowledge
+//
+//  Created by Kanz Faras on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Basic_KnowledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic_Knowledge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Basic_Knowledge
+//
+//  Created by Kanz Faras on 06/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
